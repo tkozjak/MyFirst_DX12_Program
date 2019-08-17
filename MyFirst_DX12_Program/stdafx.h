847b6f8c56a671a3de0a000d236a329e43500cf8
tkozjak <kozjaktibor@gmail.com>
2019-08-17 22:44:40 +0200

    first commit

---
+++ MyFirst_DX12_Program/stdafx.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
+#endif
+
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
+
+
+
+// handle to our window
+HWND hwnd = NULL;
+
+// name of our window class
+LPCTSTR WindowName = "DX12_Window_Clsss";
+
+// title of our window
+LPCTSTR WindowTitle = "DirectX12 Window";
+
+// width and height of our window
+int Width = 800;
+int Height = 600;
+
+// is our window full screen?
+bool FullScreen = false;
+
+
+// create a window
+bool InitializeWindow(HINSTANCE hInstance,
+	int ShowWnd,
+	int width, int height,
+	bool fullscreen);
+
+// main application loop
+void mainloop();
+
+// callback function for windows messages
+LRESULT CALLBACK WndProc(HWND hWnd,
+	UINT msg,
+	WPARAM wParam,
+	LPARAM lParam);
